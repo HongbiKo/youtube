126238b331388b052b393412de592b8407fdf5b2
HongbiKo <rhghdql666@gmail.com>
2023-12-28 21:40:47 +0900

    making search header - styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,11 +19,14 @@ export default function Header() {
 
   useEffect(()=>{setText(keyword || '')}, [keyword])
   return (
-    <header>
-      <Link to='/'><BsYoutube /><div>Youtube</div></Link>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Search...' value={text} onChange={handleChange}/>
-        <button><BsSearch /></button>
+    <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+      <Link to='/' className='flex items-center'>
+        <BsYoutube className='text-4xl text-brand'/>
+        <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
+      </Link>
+      <form className='w-full flex justify-center'onSubmit={handleSubmit}>
+        <input className='w-7/12 outline-none bg-black text-gray-50 pl-2' type="text" placeholder='Search...' value={text} onChange={handleChange}/>
+        <button className='bg-zinc-600 px-4'><BsSearch /></button>
       </form>
     </header>
   );
