44a8b4c0b394d217e1f8fe46c8421fa577b8d69e
HongbiKo <rhghdql666@gmail.com>
2023-12-28 19:30:41 +0900

    setting router from ellie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./pages/Root";
-import Videos from "./pages/Videos";
-import VideoDetail from "./pages/VideoDetail";
-import NotFound from "./pages/NotFound";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Videos /> },
-      { path: "/videos", element: <Videos /> },
-      { path: "/videos/query", element: <Videos /> },
-      { path: "/videos/watch/:videoId", element: <VideoDetail /> },
-    ],
-  },
-]);
-
-function App() {
-  return <RouterProvider router={router} />;
+export default function App() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
-
-export default App;
