e2ada98653e5bf12f86aba1c6223833f61a530c4
HongbiKo <rhghdql666@gmail.com>
2024-01-03 21:13:32 +0900

    making fakeYouTube Class

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import axios from 'axios';
+import FakeYoutube from '../api/fakeYoutube';
 
 export default function Videos() {
   const {keyword} = useParams();
   const {isLoading, error, data:videos} = useQuery({
     queryKey: ['videos', keyword],
-    queryFn: () => 
-      axios.get(`/videos/${keyword ? "search" : "popular"}.json`)
-        .then((res) => {
-          console.log(res);
-          return res.data.items;
-        })
-      ,
-  })
+    queryFn: () => {
+      const youtube = new FakeYoutube();
+      return youtube.search(keyword);
+    }},
+  )
 
   if(isLoading) return <p>loading...</p>
   if(error) return <p>{error}</p>
@@ -25,7 +22,7 @@ export default function Videos() {
       Videos {keyword ? `🔍${keyword}` : `🔥`}
       {videos && <ul>
         {
-          videos.map(video => <VideoCard key={video.id} video={video}/>)
+          videos.map(video => (<VideoCard key={video.id} video={video}/>))
         }</ul>}
     </div>
   );
