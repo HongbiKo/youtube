10c3a6f6fec17009dd60d2680f25afb28f5e3835
HongbiKo <rhghdql666@gmail.com>
2024-01-03 22:51:36 +0900

    using Context for separating query from component

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import FakeYoutube from '../api/fakeYoutube';
-import Youtube from '../api/youtube';
+// import FakeYoutube from '../api/fakeYoutube';
+// import Youtube from '../api/youtube';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
   const {keyword} = useParams();
+  const {youtube} = useYoutubeApi();
+
   const {isLoading, error, data:videos} = useQuery({
     queryKey: ['videos', keyword],
-    queryFn: () => {
-      const youtube = new Youtube();
-      return youtube.search(keyword);
-    }},
+    queryFn: () =>
+      youtube.search(keyword),
+    },
   )
 
   if(isLoading) return <p>loading...</p>
